be94baf09d0a27a7a6ba5cded9dc5ad6e2bca7de
reasure3 <reasure3@naver.com>
2022-12-26 11:07:03 +0900

    업데이트 필요 시, 채팅으로 알려주는 기능 추가

--- src/main/java/com/reasure/zomsurvival/events/ModLoggedInEvent.java
+++ src/main/java/com/reasure/zomsurvival/events/ModLoggedInEvent.java
@@ -2,7 +2,11 @@ package com.reasure.zomsurvival.events;
 
 import com.mojang.logging.LogUtils;
 import com.reasure.zomsurvival.ZomSurvival;
+import net.minecraft.ChatFormatting;
+import net.minecraft.network.chat.ClickEvent;
 import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.HoverEvent;
+import net.minecraft.network.chat.Style;
 import net.minecraft.world.entity.player.Player;
 import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -22,13 +26,31 @@ public class ModLoggedInEvent {
         ModContainer container = ModList.get().getModContainerById(ZomSurvival.MODID).orElse(null);
         if (container != null) {
             VersionChecker.CheckResult result = VersionChecker.getResult(container.getModInfo());
+            String changes = result.changes().get(result.target());
+            if (changes == null) changes = "Fail to load changelog";
             LOGGER.info("status: {}", result.status());
             LOGGER.info("changes: {}", result.changes());
             LOGGER.info("target: {}", result.target());
             LOGGER.info("url: {}", result.url());
+            Component downloadComponent = Component.literal(" [download]").withStyle(Style.EMPTY
+                    .withColor(ChatFormatting.AQUA)
+                    .withClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, result.url()))
+                    .withHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, Component.literal(result.url())))
+            );
+
+            Component changelogComponent = Component.literal(" [changes]").withStyle(Style.EMPTY
+                    .withItalic(true)
+                    .withClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/tellraw @s \"[Zombie Survivor "
+                            + result.target() + " Changelogs]\\n" + changes.replace("\n", "\\n") + "\""))
+                    .withHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, Component.literal("Click to view changelog")))
+            );
+
             if (result.status() == VersionChecker.Status.OUTDATED || result.status() == VersionChecker.Status.BETA_OUTDATED) {
-                player.displayClientMessage(Component.literal("[Zombie Survivor] There is new version."), false);
-                player.displayClientMessage(Component.literal("[Zombie Survivor] version " + result.target() + ": " + result.url()), false);
+                player.displayClientMessage(
+                        Component.literal("[Zombie Survivor] There is new version: " + result.target())
+                                .append(downloadComponent)
+                                .append(changelogComponent),
+                        false);
             }
         }
     }
